d84947cba676efe052ff6a62c6ebc733f5c99212
Ramona M <r1mari@yahoo.com>
2018-07-05 20:47:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,120 +1,54 @@
 # your code goes here
-def sort_array_asc (array)
-  array.sort
-end
-
-
-
-def sort_array_desc (array)
-  array.sort {|x,y| y <=> x }
-end
-
-
-
-def swap_elements (array)
-  array[1], array[2] = array[2], array[1]
-  array
-end
-
-
-
-def reverse_array (array)
-  array.reverse
-end
-
-
-
-def kesha_maker (array)
-  array.collect do |element|
-    element[2] = "$"
-    element
+def begins_with_r(array)
+  array.all? do |word|
+    word.downcase.chr == "r"
   end
 end
 
+def contain_a (array)
+  array.select {|element| element.include?("a")}
+end
+
+def first_wa(array)
+  array.find {|word| word.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(array)
+  array.reject {|element| element.class != String}
+end
 
 
-def greater_and_less_than_10 (array)
-  hash = {
-    "greater_than_10" => [],
-    "less_than_10" => []
-  }
-  array.each do |number|
-    if number > 10 
-      hash["greater_than_10"] << number 
-    else
-      hash["less_than_10"] << number
+def count_elements(array)
+  counts = Hash.new(0)
+  array.collect {|element| counts[element]+=1 }
+    counts.collect do |hash, number|
+      hash[:count] = number
     end
-  end
-  hash
+  counts.keys
 end
 
-
-
-def find_winners (hash)
-  winners = []
-  hash.each do |name, status| 
-    winners << name if status == "winner"
-  end
-  winners
-end
-
-
-
-def find_a (array)
-  array.select do |word|
-    word.chr == "a"
-  end
-end
-
-
-
-def sum_array (array)
-  array.inject do |sum, num|
-    sum + num
-  end
-end
-
-
-
-def add_s (array)
-  array.collect.with_index do |word, index|
-    if index == 1 
-      word 
-    else 
-      word + "s"
+def merge_data(keys, data)
+  keys.each do |name_hash|
+    data.each do |hash|
+      name_hash.merge!(hash[name_hash[:first_name]])
     end
   end
 end
 
-
-
-def count_words (words)
-  story_count = Hash.new
-
-  words.split.each do |word|
-    if story_count.include?(word)
-      story_count[word] = story_count[word] + 1
-    else
-      story_count[word] = 1
-    end
-  end
-
-  story_count
+def find_cool(array)
+  array.select {|entry| entry if entry.has_value?("cool")}
 end
 
-
-
-def organize_songs_by_artist (tracks)
-  hash = {}
-  tracks.each do |name|
-    artist = name.split(" - ")[0]
-    trackname = name.split(" - ")[1]
-
-    if hash.include?(artist)
-      hash[artist] << trackname
-    else
-      hash[artist] = [] << trackname
+def organize_schools(schools)
+  by_location = {}
+    schools.each do |school, location_hash|
+      location_hash.each do |symbol, location|
+        if by_location[location] == nil
+          by_location[location] = [school]
+        else
+          by_location[location] << school
+        end
+      end
     end
-  end
-  hash
-end 
+    by_location
+end
